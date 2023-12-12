f9b7c78ea2abd98c49edee1ae3eaa2f8613445cf
Igor Degtyarenko <div50015@mail.ru>
2023-12-12 20:42:54 +0300

    Add ios_app dir.

---
+++ README.md
@@ -0,0 +1,5 @@
+# qa guru python lesson 21
+
+-----
+
+Тесты  мобильного приложения на android с Browserstack cо сборкой в дженкинсе и  логированием в allure.
--- tests/ios_app/conftest.py
+++ tests/ios_app/conftest.py
@@ -16,26 +16,22 @@ def mobile_management():
     password = os.getenv('PASSWORD')
     options = UiAutomator2Options().load_capabilities({
         # Specify device and os_version for testing
-        # "platformName": "android",
-        "platformVersion": "9.0",
-        "deviceName": "Google Pixel 3",
+        "platformName": "ios",
+        "platformVersion": "13",
+        "deviceName": "iPhone XS",
 
         # Set URL of the application under test
-        "app": "bs://sample.app",
+        "app": "bs://bfc9cb387aa35409d0506f4fa345e2ab8fd20135",
 
         # Set other BrowserStack capabilities
         'bstack:options': {
             "projectName": "Mobile Tests Lesson 21",
-            "buildName": "browserstack-build-1",
-            "sessionName": "BStack android_test",
+            "buildName": "browserstack-build-2",
+            "sessionName": "BStack ios_test",
 
             # Set your access credentials
             "userName": f'{login}',
             "accessKey": f'{password}'
-            # "userName": "bsuser_gEb24K",
-            # "accessKey": "y2txdesxJgRFezzqc67Y"
-            # "userName": "iakivkramarenko_qKHOLN",
-            # "accessKey": "FSHAmndKHW3XsDkgm5zT"
         }
     })
 
