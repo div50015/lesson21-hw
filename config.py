c3b6e6870eedfa1cdb793e88043d77d6f517d715
Igor Degtyarenko <div50015@mail.ru>
2023-12-13 11:54:42 +0300

    Add configure.py file.

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+login = os.getenv('LOGIN', 'bsuser_gEb24K')
+password = os.getenv('PASSWORD', 'y2txdesxJgRFezzqc67Y')
--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -1,19 +1,25 @@
+import allure
 import pytest
+import allure_commons
 from appium.options.android import UiAutomator2Options
-from selene import browser
+from selene import browser, support
 import os
 from dotenv import load_dotenv
-from selenium import webdriver
+import config
+import utils
+
+from appium import webdriver
 
 
 @pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
+
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management():
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
+    # login = os.getenv('LOGIN')
+    # password = os.getenv('PASSWORD')
     options = UiAutomator2Options().load_capabilities({
         # Specify device and os_version for testing
         # "platformName": "android",
@@ -30,8 +36,8 @@ def mobile_management():
             "sessionName": "BStack android_test",
 
             # Set your access credentials
-            "userName": f'{login}',
-            "accessKey": f'{password}'
+            "userName": f'{config.login}',
+            "accessKey": f'{config.password}'
             # "userName": "bsuser_gEb24K",
             # "accessKey": "y2txdesxJgRFezzqc67Y"
             # "userName": "iakivkramarenko_qKHOLN",
@@ -39,12 +45,28 @@ def mobile_management():
         }
     })
 
-    # browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
-    browser.config.driver_remote_url = 'http://hub.browserstack.com/wd/hub'
-    browser.config.driver_options = options
+    # browser.config.driver_remote_url = 'http://hub.browserstack.com/wd/hub'
+    # browser.config.driver_options = options
+    with allure.step('init app session'):
+        browser.config.driver = webdriver.Remote(
+            "http://hub.browserstack.com/wd/hub",
+            options=options
+        )
 
     browser.config.timeout = float(os.getenv('timeout', '10.0'))
 
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext
+    )
+
     yield
 
-    browser.quit()
+    utils.allure.attach_bstack_screenshot()
+    utils.allure.attach_bstack_page_source()
+
+    session_id = browser.driver.session_id
+
+    with allure.step('tear down app session'):
+        browser.quit()
+
+    utils.allure.attach_bstack_video(session_id)
